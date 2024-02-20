329f1933eb4f9536cd1d9853b4580b0549855e71
seantan88 <seanhonourtan@gmail.com>
2024-02-20 13:37:32 -0700

    added dates

--- Data Acquisition/daily_csv_trim.py
+++ Data Acquisition/daily_csv_trim.py
@@ -7,6 +7,6 @@ import numpy as np
 df1 = pd.read_csv(r"/Users/seantan88/Documents/GitHub/ElectricityCluster/daily_merit.csv")
 
 # Want to remove every column except for the following: ID, block_number, block_price, and offer_control
-df1 = df1[['asset_ID', 'block_number', 'block_price', 'offer_control']]
+df1 = df1[['begin_dateTime_mpt','asset_ID', 'block_number', 'block_price', 'offer_control']]
 
 df1.to_csv('daily_merit_trim.csv')
